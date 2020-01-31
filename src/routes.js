a46614ccb08baefdb765e882f6d0485c336513b1
adrian-glowienka <adisonn88@wp.pl>
2020-01-31 11:55:47 +0100

    create new router to news view

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Layout from "./components/hoc/Layout/layout";
+import NewsArticle from "./components/Articles/News/Post/index";
 
 export default class routes extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class routes extends Component {
       <Layout>
         <Switch>
           <Route path="/" exact component={Home}></Route>
+          <Route path="/articles/:id" exact component={NewsArticle}></Route>
         </Switch>
       </Layout>
     );
