fc4aa709c9f49a88a170795e77055ebf84287e19
adrian-glowienka <adisonn88@wp.pl>
2020-02-11 13:10:26 +0100

    add router with videos view and linked to youtube page

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { URL } from "../../../../config";
 import styles from "../../articles.module.css";
 import Header from "./header";
-import Body from "./body";
 
 class NewsArticles extends Component {
   state = {
@@ -36,7 +35,16 @@ class NewsArticles extends Component {
           date={article.date}
           author={article.author}
         />
-        <Body />
+        <div className={styles.articleBody}>
+          <h1>{article.title}</h1>
+          <div
+            className={styles.articleImage}
+            style={{
+              background: `url('/images/articles/${article.image}')`
+            }}
+          ></div>
+          <div className={styles.articleText}>{article.body}</div>
+        </div>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Layout from "./components/hoc/Layout/layout";
 import NewsArticle from "./components/Articles/News/Post/index";
+import VideoArticle from "./components/Articles/Videos/Video/index";
 
 export default class routes extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class routes extends Component {
         <Switch>
           <Route path="/" exact component={Home}></Route>
           <Route path="/articles/:id" exact component={NewsArticle}></Route>
+          <Route path="/videos/:id" exact component={VideoArticle}></Route>
         </Switch>
       </Layout>
     );
