21e72c9151ac66c1dbc38f5a62d0fa64fdb75eff
adrian-glowienka <adisonn88@wp.pl>
2020-02-11 16:36:59 +0100

    created news main view

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Layout from "./components/hoc/Layout/layout";
 import NewsArticle from "./components/Articles/News/Post/index";
 import VideoArticle from "./components/Articles/Videos/Video/index";
+import NewsMain from "./components/Articles/News/Main/index";
 
 export default class routes extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class routes extends Component {
       <Layout>
         <Switch>
           <Route path="/" exact component={Home}></Route>
+          <Route path="/news" exact component={NewsMain}></Route>
           <Route path="/articles/:id" exact component={NewsArticle}></Route>
           <Route path="/videos/:id" exact component={VideoArticle}></Route>
         </Switch>
